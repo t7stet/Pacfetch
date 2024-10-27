0f473eddb3c6ea3bf097f8dc775901b37974e461
t7stet <im.t1stet@gmail.com>
2024-10-27 20:00:31 +0500

    Source code
    English version

---
+++ pacfetch-en.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+//Здрасте тем кто смотрит этот код :p
+
+std::string exec(const char* cmd) {
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) {
+        return "popen() failed!";
+    }
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        result += buffer;
+    }
+    pclose(pipe);
+    return result;
+}
+
+int main() {
+    std::string version = exec("pacman -V | head -n 2 | tail -n 1 | awk '{print $3}' | tail -c+2");
+    std::string totalPackages = exec("pacman -Q | wc -l");
+    std::string explicitPackages = exec("pacman -Qe | wc -l");
+    std::string availableUpdates = exec("pacman -Qu | wc -l");
+
+    std::cout << "   .--.                │ \033[0;33m Version\033[0;39m: " << version;
+    std::cout << "  / _.-' .-.  .-.  .-. │ \033[0;32m Packages\033[0;39m: " << totalPackages;
+    std::cout << "  \\  '-. '-'  '-'  '-' │ \033[0;34m Explicit packages\033[0;39m: " << explicitPackages;
+    std::cout << "   '--'                │ \033[0;31m Avalible updates\033[0;39m: " << availableUpdates << "\n";
+    return 0;
+}
